4002b71a844ca6601354ffa62a220925b0a199a3
Arteme228-hue <arti2408mac@gmail.com>
2025-05-02 12:30:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from common.bot_cmds_list import private
 ALLOWED_UPDATES = ['message, edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'))
-bot.my_admins_list = []
 
 dp = Dispatcher()
 
@@ -23,7 +22,6 @@ dp.include_router(user_private_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
